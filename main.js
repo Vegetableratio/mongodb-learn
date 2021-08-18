6aa07e0d666160b4469e31b9a220c78c7b15e819
shiki <kamiacgxu@gmail.com>
2021-08-18 17:58:16 +0800

    add nodemon&mongoose

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/yyets';
-
-MongoClient.connect(url, function (err, db) {
-  if (err) throw err;
-  console.log('数据库已创建!');
-  db.close();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('connetced');
 });
