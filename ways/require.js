a967c16ce013ecdec3f70880e5cac9226238b768
shiki <kamiacgxu@gmail.com>
2021-08-30 18:01:26 +0800

    并发控制

--- ways/require.js
+++ ways/require.js
@@ -1,7 +1,46 @@
-const result = require('../db/resource.json');
-const length = result.length;
-let arr = [];
-result.forEach(item => {
-  if (item.name.indexOf('推定有罪') !== -1) arr.push(item);
-});
-console.log(JSON.stringify(arr)); // 17517
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+const queue = require('async').queue;
+const writePath = path.resolve(__dirname, '../dist');
+const dbData = require('../db/resource.json');
+const { abort } = require('process');
+const length = dbData.length; // 17517
+const tasks = [];
+
+const main = async () => {
+  console.log(`共${length}个任务`);
+  if (!fs.existsSync(writePath)) {
+    await fsPromises.mkdir(writePath);
+  } else {
+    deleteFile(writePath);
+  }
+  dbData.forEach((dataItem, index) => {
+    q.push({ data: JSON.stringify(dataItem), fileName: path.resolve(writePath, `./${index}.json`) }, err => {
+      console.log('err=', err);
+    });
+  });
+};
+// const createFile = ({ data, fileName }) => {
+//   return fsPromises.writeFile(fileName, data, { encoding: 'utf-8' });
+// };
+const deleteFile = path => {
+  let files = [];
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path);
+    files.forEach((file, index) => {
+      const curPath = path + '/' + file;
+      if (fs.statSync(curPath).isDirectory()) {
+        deleteFile(curPath);
+      } else {
+        fs.unlinkSync(curPath);
+      }
+    });
+  }
+};
+const q = queue(({ fileName, data }, cb) => {
+  fsPromises.writeFile(fileName, data, { encoding: 'utf-8' });
+  cb();
+}, 10);
+
+length ? main() : null;
