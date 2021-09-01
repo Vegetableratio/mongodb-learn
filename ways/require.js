f7d9438b031987268dadc54dd85c1c5548a9d398
shiki <kamiacgxu@gmail.com>
2021-09-01 17:57:05 +0800

    数据格式处理

--- ways/require.js
+++ ways/require.js
@@ -14,35 +14,39 @@ const main = async () => {
   }
   console.log(`共${length}个任务`);
   console.log(`Did the queue start ? ${queue.started}`);
-  dbData.forEach((dataItem, index) => {
-    const { cnname, enname } = dataItem.data.data.info;
-    /**
-     * windows路径保留符号
-      <（小于）
-      >（大于）
-      ： (冒号)
-      "（双引号）
-      /（正斜杠）
-      \ (反斜杠)
-      | (竖线或管道)
-      ? （问号）
-      * (星号)
-     */
-    const name = `${index + 1}：${cnname}_${enname}`.replace(/[\<\>\:\"\/\\\|\?\*\t\n\r]/g, '');
-    const task = {
-      data: JSON.stringify(dataItem, null, 2),
-      fileName: path.resolve(writePath, `./${name}.json`),
-      name,
-      index,
-    };
-    queue.push(task, (error, { task, remaining }) => {
-      if (error) {
-        console.log(`An error occurred while processing task ${task.index}`, error);
-        error.name = task.name;
-        errArry.push(error);
-      }
-    });
+  const l = dbData.map(dataItem => {
+    return handleData(dataItem);
   });
+  _writeFile(path.resolve(__dirname, `../log/l.json`), JSON.stringify(l, null, 2));
+  // dbData.forEach((dataItem, index) => {
+  //   const { cnname, enname } = dataItem.data.data.info;
+  //   /**
+  //    * windows路径保留符号
+  //     <（小于）
+  //     >（大于）
+  //     ： (冒号)
+  //     "（双引号）
+  //     /（正斜杠）
+  //     \ (反斜杠)
+  //     | (竖线或管道)
+  //     ? （问号）
+  //     * (星号)
+  //    */
+  //   const name = `${index + 1}：${cnname}_${enname}`.replace(/[\<\>\:\"\/\\\|\?\*\t\n\r]/g, '');
+  //   const task = {
+  //     data: JSON.stringify(dataItem, null, 2),
+  //     fileName: path.resolve(writePath, `./${name}.json`),
+  //     name,
+  //     index,
+  //   };
+  //   queue.push(task, (error, { task, remaining }) => {
+  //     if (error) {
+  //       console.log(`An error occurred while processing task ${task.index}`, error);
+  //       error.name = task.name;
+  //       errArry.push(error);
+  //     }
+  //   });
+  // });
   console.log(`Did the queue start ? ${queue.started}`);
 };
 
@@ -110,4 +114,36 @@ _writeFile(path.resolve(__dirname, `../log/nameList.json`), JSON.stringify(nameL
   .catch(error => {
     console.log('error', error);
   });
+// 数据处理
+const handleData = dataItem => {
+  const {
+    data: {
+      data: { info, list },
+    },
+  } = dataItem;
+  const newList = list.map(listItem => {
+    const { season_cn, items, formats } = listItem;
+    const newList = [];
+    formats.forEach(format => {
+      if (format !== `APP`) {
+        const fileCollect = {};
+        items[format].forEach(detail => {
+          detail.files &&
+            detail.files.forEach(file => {
+              fileCollect[format] ? null : (fileCollect[format] = {});
+              fileCollect[format][file.way_cn]
+                ? fileCollect[format][file.way_cn].push(file.address)
+                : (fileCollect[format][file.way_cn] = []);
+            });
+        });
+        newList.push(fileCollect);
+      }
+    });
+
+    const newItem = { season_cn, downLoadLink: newList };
+    return newItem;
+  });
+  const data = { info, newList };
+  return data;
+};
 length ? main() : null;
